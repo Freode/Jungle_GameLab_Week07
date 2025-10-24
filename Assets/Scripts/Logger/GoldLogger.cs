c7f4fd6d8a8d6ed6e67e062c3d96fb384862cab8
Choi Jeongho <jeonghoe22@gmail.com>
2025-10-24 13:59:13 +0900

    feat : 현재 클릭 & 초당 금 획득량 로그 추가

--- Assets/Scripts/Logger/GoldLogger.cs
+++ Assets/Scripts/Logger/GoldLogger.cs
@@ -1,42 +1,66 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GoldLogger : MonoBehaviour
 {
-    int sequence = 1;
-    decimal acquireNormalGoldAmount = 0;
-    decimal acuqireAutoGoldAmount = 0;
+    int accumulateSequence = 1;
+    decimal accumulateNormalGoldAmount = 0;
+    decimal accumulateAutoGoldAmount = 0;
+
+    int acquireSequence = 1;
 
     private void Start()
     {
-        StartCoroutine(UpdateGoldLog());
+        StartCoroutine(UpdateAccumulateGoldLog());
+        StartCoroutine(UpdateAcquireGoldLog());
     }
 
-    IEnumerator UpdateGoldLog()
+    IEnumerator UpdateAccumulateGoldLog()
     {
+        GameLogger.Instance.Log("AccumulateGold", $"====== Interval : 20s ======");
         while (true)
         {
             yield return new WaitForSeconds(20f);
 
-            decimal totalGoldAmount = acquireNormalGoldAmount + acuqireAutoGoldAmount;
+            decimal totalGoldAmount = accumulateNormalGoldAmount + accumulateAutoGoldAmount;
             if (totalGoldAmount == 0) totalGoldAmount = 1;
 
-            GameLogger.Instance.Log("GoldAcquirement", $"====== Sequence : {sequence}번 ======");
-            GameLogger.Instance.Log("GoldAcquirement", $"[Total Gold Acquirement : {FuncSystem.Format(totalGoldAmount)}] [Real Value : {totalGoldAmount:F0}] [Rate : 100%]");
-            GameLogger.Instance.Log("GoldAcquirement", $"[Normal Gold Acquirement : {FuncSystem.Format(acquireNormalGoldAmount)}] [Real Value : {acquireNormalGoldAmount:F0}] [Rate : {acquireNormalGoldAmount / totalGoldAmount * 100:F2}%]");
-            GameLogger.Instance.Log("GoldAcquirement", $"[Auto Gold Acquirement : {FuncSystem.Format(acuqireAutoGoldAmount)}] [Real Value : {acuqireAutoGoldAmount:F0}] [Rate : {acuqireAutoGoldAmount / totalGoldAmount * 100:F2}%]");
+            GameLogger.Instance.Log("AccumulateGold", $"====== Sequence : {accumulateSequence}번 ======");
+            GameLogger.Instance.Log("AccumulateGold", $"[Total Gold Accumulate : {FuncSystem.Format(totalGoldAmount)}] [Real Value : {totalGoldAmount:F0}] [Rate : 100.00%]");
+            GameLogger.Instance.Log("AccumulateGold", $"[Normal Gold Accumulate : {FuncSystem.Format(accumulateNormalGoldAmount)}] [Real Value : {accumulateNormalGoldAmount:F0}] [Rate : {accumulateNormalGoldAmount / totalGoldAmount * 100:F2}%]");
+            GameLogger.Instance.Log("AccumulateGold", $"[Auto Gold Accumulate : {FuncSystem.Format(accumulateAutoGoldAmount)}] [Real Value : {accumulateAutoGoldAmount:F0}] [Rate : {accumulateAutoGoldAmount / totalGoldAmount * 100:F2}%]");
 
-            ++sequence;
+            ++accumulateSequence;
+        }
+    }
+
+    IEnumerator UpdateAcquireGoldLog()
+    {
+        GameLogger.Instance.Log("AcquireGold", $"====== Interval : 5s ======");
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+
+            decimal acquireClick = GameManager.instance.GetClickIncreaseTotalAmount();
+            decimal acquireAuto = GameManager.instance.GetPeriodIncreaseTotalAmount();
+            decimal totalAcquire = acquireAuto + acquireClick;
+            GameLogger.Instance.Log("AcquireGold", $"====== Sequence : {acquireSequence}번 ======");
+            GameLogger.Instance.Log("AcquireGold", $"[Total Gold Accumulate : {FuncSystem.Format(totalAcquire)}] [Real Value : {totalAcquire:F0}] [Rate : 100.00%]");
+            GameLogger.Instance.Log("AcquireGold", $"[Normal Gold Accumulate : {FuncSystem.Format(acquireClick)}] [Real Value : {acquireClick:F0}] [Rate : {acquireClick / totalAcquire * 100:F2}%]");
+            GameLogger.Instance.Log("AcquireGold", $"[Auto Gold Accumulate : {FuncSystem.Format(acquireAuto)}] [Real Value : {acquireAuto:F0}] [Rate : {acquireAuto / totalAcquire * 100:F2}%]");
+
+            ++acquireSequence;
         }
     }
 
     public void AcquireAutoGoldAmount(long amount)
     {
-        acuqireAutoGoldAmount += amount;
+        accumulateAutoGoldAmount += amount;
     }
 
     public void AcquireNormalGoldAmount(long amount)
     {
-        acquireNormalGoldAmount += amount;
+        accumulateNormalGoldAmount += amount;
     }
 }
